f97b324acc98b9f83ed359d3ed4045769f57118b
MrGreque <64713749+MrGreque@users.noreply.github.com>
2020-09-08 23:12:53 -0300

    Classes e View (Inicial e Tela Funcionários)*

---
+++ src/classes/Funcionario.java
@@ -0,0 +1,18 @@
+package classes;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Funcionario {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
