f4fc0ff34f46eaf308f560dedeacb863f2c78743
MrGreque <64713749+MrGreque@users.noreply.github.com>
2020-09-09 16:31:52 -0300

    Classes

---
+++ GerenciamentoFuncionario/src/classes/Funcionario.java
@@ -0,0 +1,18 @@
+package classes;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Funcionario extends Cargo{
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
